ba90cc846bb44240c08a35b4bc9efa92c8047ab9
Megan <meganepetty@gmail.com>
2023-10-24 14:00:00 -0600

    notes on java as well as just messing around experimenting with java

---
+++ script.js
@@ -0,0 +1,37 @@
+// in-line comment
+
+
+console.log("Hello World");
+
+/* multi
+line
+comment
+*/
+
+//declare a variable
+var myName = "Megan";
+//can be used anywhere in the code
+
+myName = 8;
+
+let ourName = "freeCodeCamp";
+//let is only used within the scope it's defined in
+
+const pi = 3.14;
+//variable cannot and will not change
+
+//declaring vs assigning
+var a; //declaring
+
+var b = 2; //assigning
+
+var sum = 10 + 10;
+console.log(sum);
+
+myVar = 87;
+myVar++;
+
+//append data
+
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"],)
